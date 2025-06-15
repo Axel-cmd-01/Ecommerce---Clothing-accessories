c3e7dd86607ab7eb6773d040b27ad3acf7716072
Lazaro Juarez <lazarojuarez.h@outlook.com>
2025-06-15 01:06:32 -0600

    Primer commit del proyecto

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Clothing accessories</title>
+
+  <link rel="icon" href="assets/images/Logo.png" type="image/png">
+</head>
+
+<body>
+  <h1>Hola, este es el inicio de mi tienda en la que voy a estar trabajando durante unos mb_ereg_search_setpos</h1>
+</body>
+
+</html>
