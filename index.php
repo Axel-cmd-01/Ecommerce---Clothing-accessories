eb53eb82a3447f4fdbf2d8bd218cee36df8826a2
Lazaro Juarez <lazarojuarez.h@outlook.com>
2025-06-18 02:19:19 -0600

    Modifique la primer parte del main

--- index.php
+++ index.php
@@ -14,11 +14,22 @@
   <!-- Favicon para modo oscuro -->
   <link rel="icon" href="assets/images/Logo-white.png" media="(prefers-color-scheme: dark)">
 
+  <link rel="stylesheet" href="assets/css/style.css">
+
+<!-- Estilo de la fuente-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Chiron+Sung+HK:ital,wght@0,200..900;1,200..900&family=Josefin+Sans:ital@0;1&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
   <?php include 'includes/header.php'; ?>
 
+  <?php include 'pages/home.php'; ?>
+
+
+
+  <script src="../assets/js/main.js"></script>
 
 </body>
 
---
+++ pages/home.php
@@ -0,0 +1,15 @@
+<main>
+  <section class="first-section w-full flex relative h-[calc(100vh-80px)] overflow-hidden bg-[#242424] text-white">
+    <div class="left-side w-1/3 flex items-center p-10">
+      <h1 class="text-8xl">OVERSIZE<br>STYLE</h1>
+    </div>
+
+    <div class="right-side w-2/3 ">
+      <img class="imagen-playeras" src="../assets/images/imagen-de-playeras.jpg" alt="Imagen de playeras">
+    </div>
+  </section>
+
+  <section class="second-section">
+    <h1>parte dos del main</h1>
+  </section>
+</main>
